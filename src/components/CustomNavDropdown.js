a58d79eb010f0746d96d7349a898d31b713be34a
VinayKumar <yashwinin58@gmail.com>
2021-07-11 19:56:06 +0530

    menu component changes

--- src/components/CustomNavDropdown.js
+++ src/components/CustomNavDropdown.js
@@ -4,17 +4,20 @@ import { Nav, Navbar, NavDropdown, Form, FormControl, Button } from 'react-boots
 
 	const eventlistenerhandler= function(evt) {
         let flyoutEl = document.querySelector('.dropdown_container'),
-        flyoutElText = document.querySelectorAll('.custom_dropdown .nav-link'),
+        flyoutElText = document.querySelectorAll('.custom_dropdown'),
         childelem = document.querySelectorAll('.dropdown_container.children'),
           targetEl = evt.target; // clicked element   
 			let somefilter   = Array.prototype.some,
 				result   = flyoutElText,
 				filtered = somefilter.call( result, function( node ) {
+
+                    // console.log("targetel",targetEl)
 				return node==targetEl;
 			});
+                console.log("targetel",flyoutEl,"=====",targetEl)
 				// console.log('flyoutElText',flyoutElText);
         do {
-          if(targetEl == flyoutEl||filtered) {
+          if((targetEl == flyoutEl)||filtered) {
           	console.log("clicking inside")
           	return;
           }
@@ -33,19 +36,21 @@ import { Nav, Navbar, NavDropdown, Form, FormControl, Button } from 'react-boots
         // document.getElementById("flyout-debug").textContent = "Clicked outside!";
       }
     const getboundingpoints = (e,title="",children,dropdowns) => {
-    	console.log("times",dropdowns)
+        e.preventDefault();
     	// document.removeEventListener("click",eventlistenerhandler);
         let boundingpoints = e.currentTarget.getBoundingClientRect();
         let boundingpointsForParent = document.querySelector('.dropdown_container.parent')&&document.querySelector('.dropdown_container.parent').getBoundingClientRect();
         console.log('boundingpoints',boundingpoints);
+        let lastdropdown=!dropdowns || (dropdowns&&dropdowns.length==0);
         let createElem = document.createElement('div');
-        createElem.setAttribute('class', `dropdown_container ${children?'children':'parent'}`);
+        createElem.setAttribute('class', `dropdown_container ${children?`children ${lastdropdown?'last':''}`:'parent'}`);
         let getparentWidth=boundingpointsForParent?boundingpointsForParent.width:0;
         createElem.style.top=(children?(boundingpoints.y):(boundingpoints.top+boundingpoints.height+boundingpoints.y))+"px";
         createElem.style.left=(children?((getparentWidth+boundingpoints.left)):boundingpoints.left)+"px";
-        
         let checkDom = document.querySelector('.dropdown_container');
         let childelem = document.querySelectorAll('.dropdown_container.children');
+
+ 
         if (checkDom) {
         	if(!children){	
         	document.body.replaceChild(createElem,checkDom);
@@ -55,7 +60,6 @@ import { Nav, Navbar, NavDropdown, Form, FormControl, Button } from 'react-boots
 				result   = childelem,
 				somefilterfunction  =(elems,targetEl)=> {
 					// return true;
-					console.log("elems",elems,targetEl);
 				let somefilter=Array.prototype.some,
 				filtered = somefilter.call( elems, function( node ) {
 				return node==targetEl;
@@ -71,10 +75,12 @@ import { Nav, Navbar, NavDropdown, Form, FormControl, Button } from 'react-boots
 				}
 				});
         	}
+            if(dropdowns){
                   ReactDOM.render(
-                <Dropdown dropdowns={dropdowns} title={title}/>,
-                children?document.body.appendChild(createElem):createElem
-            );
+                    <Dropdown dropdowns={dropdowns} title={title}/>,
+                    children?document.body.appendChild(createElem):createElem
+                );
+              }
 
         	// },2000)
         } else {
@@ -108,7 +114,7 @@ export const Dropdown = ({title,dropdowns}) => {
     return (
         <div className="dropdown_layer">
         {dropdowns.map((obj)=>{
-        	return obj.children? <NavDropdown.Item className="custom_dropdown" onMouseOver={(e)=>getboundingpoints(e,title,true,obj.children)} onClick={(e)=>getboundingpoints(e,title,true,obj.children)}>{obj.title}</NavDropdown.Item>:<NavDropdown.Item >{obj.title}</NavDropdown.Item>
+        	return obj.children? <NavDropdown.Item className="custom_dropdown" onMouseOver={(e)=>getboundingpoints(e,title,true,obj.children)} onClick={(e)=>getboundingpoints(e,title,true,obj.children)}>{obj.title}</NavDropdown.Item>:<NavDropdown.Item className="custom_dropdown" onMouseOver={(e)=>getboundingpoints(e,title,true,null)}>{obj.title}</NavDropdown.Item>
         })
         }	 
 		</div>
